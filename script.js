a40da4674236c89d01aea593b195542374dfadfe
Kaua Lima <kaualima9@gmail.com>
2023-07-26 21:57:58 -0300

    Instruções iniciais JS e git

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+Introdução git e JavaScript
+
+*/
+
+
+
+var nome = "Kaua"
+let idade = "20"
+const anoNascimento = 2003
+let idadeEsposa = "20" 
+
+/*
+= atribuição 
+== comparar se é igual
+=== comparar se é exatamente igual
+*/
+
+let listaCompras = []
+
+listaCompras.push("Maça")
+
+console.log(listaCompras.length)
+
+console.log(listaCompras)
